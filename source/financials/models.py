fcdca7351bbad6015c687fb72a640db17f8a5326
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2023-09-22 22:50:40 +0300

    create CostSerializer in serializers.py

--- source/financials/models.py
+++ source/financials/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from products.validators import validate_price
 
-# Create your models here.
+class Cost(models.Model):
+    description = models.CharField(verbose_name= 'الوصف',
+                                   max_length=400,
+                                   null=False,
+                                   blank=False)
+    price = models.DecimalField(max_digits=8,
+                                decimal_places=2,
+                                null=False,
+                                blank=False,
+                                validators=[validate_price])
+    date = models.DateTimeField(verbose_name="وقت الدفع",
+                                auto_now_add=True)
+    # user = models.ForeignKey(User, verbose_name='مسئول الشيفت', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        verbose_name = "Cost"
+        verbose_name_plural = "Costs"
+        db_table = "costs" 
+        ordering = ['-id']
---
+++ source/financials/urls.py
@@ -0,0 +1,5 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+router = DefaultRouter
+router.register(r'costs', )
