bba79bc87035ff8dc02d5648de0253215192305e
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2023-09-22 22:52:24 +0300

    create CostViewSet class in views.py

--- source/financials/views.py
+++ source/financials/views.py
@@ -1,2 +1,14 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from .models import Cost
+from .serializers import CostSerializer
 
-# Create your views here.
+class CostViewSet(ModelViewSet):
+    queryset = Cost.objects.all()
+    serializer_class = CostSerializer
+    # authentication_classes = None
+    # permission_classes = None
+    filter_backends = [SearchFilter, ]
+    search_fields = ['date', 'description']
+
