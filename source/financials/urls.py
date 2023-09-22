12eb4e0202f1b56170c95458942c20b318dbdfe6
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2023-09-22 22:54:24 +0300

    add urls.py file with router in financials/urls.py

--- source/financials/urls.py
+++ source/financials/urls.py
@@ -1,5 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from .views import CostViewSet
 
 router = DefaultRouter
-router.register(r'costs', )
+router.register(r'costs', CostViewSet, basename='costs' )
+urlpatterns=[
+    path('', include(router.urls))
+]
