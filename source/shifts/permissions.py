49fa513b9500f92d56722ca666f0d30e16f68588
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2023-10-10 05:31:45 +0300

    add patch method in shift employee permissions

--- source/shifts/permissions.py
+++ source/shifts/permissions.py
@@ -22,7 +22,7 @@ class IsEmployee(BasePermission):
     def has_permission(self, request, view):
 
         # The employee is allowed to retrieve his shift only, GET IN GENERAL is not allowed
-        if request.method in('POST', 'PATCH') or view.action == "retrieve":
+        if request.method in ('POST', 'PATCH') or view.action == "retrieve":
             return request.user.role == "EMPLOYEE"
         
         else:
