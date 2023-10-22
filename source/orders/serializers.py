a893573c22781e770f5ab618e5037a6e7d311ff9
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2023-10-23 01:14:32 +0300

    adding to_represntative for customer in OrderGetSerializer

--- source/orders/serializers.py
+++ source/orders/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from customers.serializers import CustomerSerializer
 from .models import Order, OrderItem
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -33,7 +35,16 @@ class OrderGetSerializer(serializers.ModelSerializer):
     shift = serializers.StringRelatedField()
     
     # Here we make stringRelated() to retrieve the customer to show it on the customer screen
-    customer = serializers.StringRelatedField()
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        customer_instance = instance.customer
+        if customer_instance:
+            representation["customer"] = {
+                "id":customer_instance.id,
+                "name": customer_instance.full_name
+            }
+        return representation
 
     total_price = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
     created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M', required=False)
